0a9ede9448a4a6d8dca145b69cd1396020f95352
Eng-soke <cabdicasiismaxamed273@gmail.com>
2025-04-08 14:32:33 +0300

    waxan ka saraya services

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
-import Services from "./pages/services"
+import Services from "./pages/Services"
 function App() {
   return <div>
     
--- src/pages/Services.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Header from "../components/Header";
-function Services() {
-    return <div>
-        <Header />
-        <h1>This is page is services</h1>
-    </div>
-}
-export default Services
