b4d1bf15310269224ddb3373e28beb4146d1c991
Eng-soke <cabdicasiismaxamed273@gmail.com>
2025-04-08 14:55:11 +0300

    waxaan bedelay

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from "../components/Hero"
 import ShowCase from "../components/ShowCase"
 import Body from "../components/Body"
 import WhyChooseUs from "../components/WhyChooseUs"
-import BestCompony from "../components/bestCompony"
+import BestCompony from "../components/BestCompony"
 import Footer from "../components/Footer"
 
 function Home() {
