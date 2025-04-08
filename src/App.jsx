a1c10a196c6af7a4598ef5156c0314691a94d2eb
Eng-soke <cabdicasiismaxamed273@gmail.com>
2025-04-08 14:35:37 +0300

    waxaan

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react"
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
-import Services from "./pages/Services"
 function App() {
   return <div>
     
     <Routes>
 
       <Route path="/" element={<Home/>} />
-      <Route path="/services" element={<Services/>} />
-
 
     </Routes>
 
